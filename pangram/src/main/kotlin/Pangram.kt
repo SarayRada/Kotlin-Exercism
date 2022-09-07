777a269ce6b11fe48e068ea41ed49367a4e5d6b8
Saray Rada <SarayRadaReboso@outlook.es>
2022-09-07 14:59:38 +0200

    pangram

---
+++ pangram/src/main/kotlin/Pangram.kt
@@ -0,0 +1,26 @@
+import java.util.*
+
+object Pangram {
+    fun isPangram(input: String): Boolean {
+        if (input.isEmpty()) return false
+        var hasTheLetter: Boolean = false
+        val compare = input.toCharArray().apply {sort()}.filterNot { it == '"'}.filterNot { it == ' ' }
+        val abecedary = "abcdefghijklmnopqrstuvwxyz".toCharArray()
+        for (letter in abecedary){
+            for (letters in compare) {
+                hasTheLetter = false
+                if (letters.toLowerCase() == letter) {
+                    hasTheLetter = true
+                    break
+                }
+            }
+            if (!hasTheLetter) return hasTheLetter
+        }
+        return hasTheLetter
+    }
+}
+
+fun main (){
+    println(Pangram.isPangram("\"Five quacking Zephyrs jolt my wax bed.\""))
+
+}
