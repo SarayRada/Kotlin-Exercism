fc7b13f9ab34d9e09d4926b577c81715524ef68f
Saray Rada <SarayRadaReboso@outlook.es>
2022-09-08 14:42:32 +0200

    mejorando lectura

--- isogram/src/main/kotlin/Isogram.kt
+++ isogram/src/main/kotlin/Isogram.kt
@@ -1,10 +1,10 @@
 object Isogram {
     fun isIsogram(input: String): Boolean {
-        val myInputToChar = input.lowercase().toCharArray().filter { it -> it.isLetter() }
-        val myInputWithNotRepeatable = myInputToChar.distinct()
-        return myInputToChar.size == myInputWithNotRepeatable.size
+        val onlyLetters = input
+            .lowercase()
+            .toCharArray()
+            .filter { it.isLetter() }
+        val nonRepeatedLetters = onlyLetters.distinct()
+        return onlyLetters.size == nonRepeatedLetters.size
     }
-}
-fun main(){
-    println(Isogram.isIsogram("ghsyhhs"))
 }
