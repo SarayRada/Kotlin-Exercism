e98186b81172f95a68da298e53761584b420fcd7
Saray Rada <SarayRadaReboso@outlook.es>
2022-09-09 18:50:49 +0200

    git latin

--- pig-latin/src/main/kotlin/PigLatin.kt
+++ pig-latin/src/main/kotlin/PigLatin.kt
@@ -1,19 +1,25 @@
 object PigLatin {
     fun translate(phrase: String): String {
-        val vowelsSound: List<String> = listOf("a", "e", "i", "o", "u", "xr", "yt")
+        val vowelsSound: List<String> = listOf("a", "e", "i", "o", "u")
         val consonant: List<Char> = listOf('b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z')
-        if (vowelsSound.any { phrase.startsWith(it)}) {
-            return "${phrase}ay"
-        } else if (vowelsSound.any{phrase.}){
-            return "ay"
-        } else if (consonant.any{phrase.startsWith(it)}){
-            val phraseNotFirst = phrase.substring(1, phrase.length)
-            return "${phraseNotFirst}${phrase.first()}ay"
-        }
 
+        if (vowelsSound.any { phrase.startsWith(it)} || phrase.startsWith("xr") || phrase.startsWith("yt")) {
+            return "${phrase}ay"
+        } else if (consonant.any{phrase.startsWith(it)|| phrase.startsWith("qu")}){
+            val initialConsonants: String
+            val wordsLast: String
+                if (phrase.startsWith("qu")) {
+                    initialConsonants = "qu"
+                    wordsLast = phrase.substringAfter(initialConsonants)
+                } else {
+                    initialConsonants = phrase.takeWhile {consonant.contains(it)} 
+                    wordsLast = phrase.substringAfter(initialConsonants)
+                }
+            return "${wordsLast}${initialConsonants}ay"
+            }
         return "lullaby"
     }
 }
 fun main (){
-    println(PigLatin.translate("rala"))
+    println(PigLatin.translate("ytry"))
 }
